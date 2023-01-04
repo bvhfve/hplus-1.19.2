43bd185482f8477cae0088a6e4cbb11ce3fb2a1d
Cong Dat Tran <95630070+bvhfve@users.noreply.github.com>
2023-01-04 22:23:50 +0700

    Idk

--- Fabric/src/main/java/net/flytre/hplus/Initializer.java
+++ Fabric/src/main/java/net/flytre/hplus/Initializer.java
@@ -1,10 +1,1 @@
-package net.flytre.hplus;
 
-import net.fabricmc.api.ModInitializer;
-
-public class Initializer implements ModInitializer {
-    @Override
-    public void onInitialize() {
-        Registry.init();
-    }
-}
